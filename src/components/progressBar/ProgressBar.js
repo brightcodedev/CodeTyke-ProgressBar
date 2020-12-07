c2aca17ffe856d2a7b5e274ab8830bf679a3111d
Jonah Ullman <jonahmullman@gmail.com>
2020-12-07 17:56:10 -0600

    Implements width logic and styling for ProgressBar

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -3,11 +3,17 @@ import React from 'react'
 import './Styles.scss'
 
 const ProgressBar = ({quizData, currentQuestionId}) => {
+  const progressBarWidth = String(((currentQuestionId + 1) / (quizData.totalQuestions + 1)) * 100) + "%"
+  console.log(progressBarWidth)
+  const progressBarStyle = {
+    width: progressBarWidth
+  }
   return (
-    <>
-      <div className="progressBar progressBar--incomplete"></div>
-      <div className="progressBar progressBar--complete"></div>
-    </>
+    <div className="progressBar">
+      <div className="progressBar--incomplete">
+        <div className="progressBar--complete" style={progressBarStyle}></div>
+      </div>
+    </div>
   )
 
 }
