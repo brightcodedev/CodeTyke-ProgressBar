4f3b909df3669936fef6ccfdaad329500e9f59a2
21shield <21netaly@gmail.com>
2020-12-08 16:32:26 -0500

    added initial bar no info

---
+++ src/components/progressBar/progressBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Styless.scss";
+
+export default function progressBar() {
+  // const progressStyle = {
+  //     width: 25%;
+  // }
+
+  return (
+    <div className="meter">
+      <span>progress</span>
+      {/* style={progressStyle} */}
+    </div>
+  );
+}
