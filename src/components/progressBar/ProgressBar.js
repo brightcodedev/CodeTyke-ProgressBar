34cd9a2dc763a1ba1d56e65932cce4c19a1403df
Jonah Ullman <jonahmullman@gmail.com>
2020-12-07 19:53:19 -0600

    Adds dot to right side of progress bar

--- src/components/progressBar/ProgressBar.js
+++ src/components/progressBar/ProgressBar.js
@@ -4,15 +4,16 @@ import './Styles.scss'
 
 const ProgressBar = ({quizData, currentQuestionId}) => {
   const progressBarWidth = String(((currentQuestionId + 1) / (quizData.totalQuestions + 1)) * 100) + "%"
-  console.log(progressBarWidth)
   const progressBarStyle = {
     width: progressBarWidth
   }
+
   return (
     <div className="progressBar">
       <div className="progressBar--incomplete">
         <div className="progressBar--complete" style={progressBarStyle}></div>
       </div>
+      <div className="progressBar__dot"></div>
     </div>
   )
 
